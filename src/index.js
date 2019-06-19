8917cecc384010c2f63ac3e66c1f95439ba553e9
prewe <45303311+prewe@users.noreply.github.com>
2019-06-19 23:05:22 +0700

    add mvc

--- src/index.js
+++ src/index.js
@@ -1,14 +1,73 @@
-import './modules/jq'
-import './css/index.css'
+import $ from 'jquery'
 import './scss/index.scss'
-import AppService from './modules/ser'
+
+class EventEmitter {
+  constructor() {
+    this._events = {};
+  }
+  on(evt, listener) {
+    (this._events[evt] || (this._events[evt] = [])).push(listener);
+    return this;
+  }
+  emit(evt, arg) {
+    (this._events[evt] || []).slice().forEach(lsn => lsn(arg));
+  }
+}
+
+class Model extends EventEmitter {
+  constructor(options) {
+    super();
+    this._settings = $.extend({min: 0, max: 10, step: 1, value: 0}, options);
+  }
+
+  log() {
+    console.log(this._settings);
+  }
+}
+
+class View extends EventEmitter {
+  constructor(model) {
+    super();
+    this._model = model;
+    this._element = $('.slider');
+    this._slider = $(`<input type="range" min="${this._model._settings.min}" max="${this._model._settings.max}" step="${this._model._settings.step}" value="${this._model._settings.value}">`);
+
+    this._slider.on('input', e => this.emit('valueChanged', e.target.value));
+  }
+
+  
+
+  createSlider() {
+    this._element.append(this._slider);
+  }
+
+  log() {
+    console.log(this._element);
+  }
+}
+
+class Controller {
+  constructor(model, view) {
+    this._model = model;
+    this._view = view;
+
+    view.on('valueChanged', value => this._model._settings.value = value);
+  }
+
+  getValue() {
+    console.log(this._model._settings.value);
+  }
+}
+
+const model = new Model({value: 3})
+const view = new View(model)
+const controller = new Controller(model, view)
+model.log();
+view.log();
+view.createSlider();
+controller.getValue();
 
 
 
-console.log('hello world!');
-console.log('hello world2!');
 
-const ser = new AppService('hello man!!!!!');
-
-ser.log();
 
--- src/modules/jq.js
+++
@@ -1,3 +0,0 @@
-import $ from 'jquery'
-
-$('.box').css('background', 'green').css('color', 'red').append('<h1>Hello</h1>');

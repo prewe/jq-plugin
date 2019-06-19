bc596bfbcd34a8b8f443cd8b640a84b9d86c9b81
prewe <45303311+prewe@users.noreply.github.com>
2019-06-19 21:31:45 +0700

    configured webpack

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import './modules/jq'
+import './css/index.css'
+import './scss/index.scss'
+import AppService from './modules/ser'
+
+
+
+console.log('hello world!');
+console.log('hello world2!');
+
+const ser = new AppService('hello man!!!!!');
+
+ser.log();
+
---
+++ src/modules/jq.js
@@ -0,0 +1,3 @@
+import $ from 'jquery'
+
+$('.box').css('background', 'green').css('color', 'red').append('<h1>Hello</h1>');
